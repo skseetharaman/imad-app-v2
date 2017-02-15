ddfebaea62bca7054872ebfd345edd4b7092b2bb
skseetharaman <skseetharaman@gmail.com>
2017-02-15 17:28:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,12 +33,11 @@ function createTemplate(data){
     
     var htmlTemplate =`
     <html>
-      <head>
-        <title>
-          $title
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/ui/style.css" rel="stylesheet" />
+      <head>  
+            <title> ${title}</title>
+             
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
           <div class="container">
@@ -58,10 +57,8 @@ function createTemplate(data){
         </body>
       
     </html>
-    `   ;
+    `;   
     return htmlTemplate;
-
-
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
