e59af08a16a6a24bd839ace59af630241f897197
skseetharaman <skseetharaman@gmail.com>
2017-02-15 22:21:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/:articleName', function (req, res){
 app.get('/:articleName', function (req, res){
  
       //var articleName = req.params.articleName;
-      res.send(createTemplate(articleTwo));
+      res.send(createTemplate(articleOne));
 });
 
 
